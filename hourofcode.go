e452a0d362e22f637e454f3d5680d4fb47fe47f9
marcus holmes <mh@marcusholmes.biz>
2014-12-02 11:41:16 +0800

    fix typo in go description

--- hourofcode.go
+++ hourofcode.go
@@ -1,4 +1,5 @@
 //home/marcus/.gvm/gos/release/bin/go run $0 $@ ; exit
+// you'll need to replace the above path with your $GOROOT path
 package main
 
 import (
@@ -7,8 +8,8 @@ import (
 )
 
 func main() {
-	var name = flag.String("name", "world", "who are we saying hello to?")
-	flag.StringVar(name, "n", "world", "who are we saying hellow to?")
+	name := flag.String("name", "world", "who are we saying hello to?")
+	flag.StringVar(name, "n", "world", "who are we saying hello to?")
 	flag.Parse()
 	fmt.Printf("Hello %s!\n", *name)
 }
